cmss13-devs/cmss13

/tg/ Status Effects Part 2 - datum, KD, KO, Stuns (#4842)
# About the pull request

<!-- Remove this text and explain what the purpose of your PR is.

Mention if you have tested your changes. If you changed a map, make sure
you used the mapmerge tool.
If this is an Issue Correction, you can type "Fixes Issue #169420" to
link the PR to the corresponding Issue number #169420.

Remember: something that is self-evident to you might not be to others.
Explain your rationale fully, even if you feel it goes without saying.
-->

Part 2 - this includes porting the actual status_effect datum, modifying
it to fit our purposes by backing it with timers similarly to old
system, and finally implementing KD, KO and Stun with it.

This contains Part 1 PR (#4828) so if you want to take a look at it I'd
advise checking the last commits or setting up a compare between both
branches.

# Explain why it's good for the game
Predictable status timers. Current ones are bogus in their handling of
"life tick correction" and will "stack" time even when they're not
supposed to.

Also provides a more robust backend for general effects, and integrates
status effects into it.

# Testing Photographs and Procedure

Summary testing of buckling interactions, explosion knock times,
crawling, resting. Will have to be expanded once part 1 is ready


# Changelog
:cl:
add: Added Buckled, Handcuffed and Legcuffed screen alerts
code: Ported /tg/ status effects backend, modified with timers to let
effects end at appropriate time
code: Stun, Knockdown and Knockout now use the new effects backend
balance: Due to backend change, all KO/KD/Stuns may behave differently
timing wise. This is of course subject to adjustments.
balance: Endurance is now set at 8% effect duration reduction per level
above 1. However it now compounds with species bonus. Feel free to
adjust.
balance: Knockdowns are not inherently incapacitating anymore and many
sources of it have been updated to also stun to make up for it.
fix: KO/KD/Stun

code/modules/mob/living/carbon/xenomorph/abilities/warrior/warrior_powers.dm
@@ -77,9 +77,9 @@
 	xeno.visible_message(SPAN_XENOWARNING("[xeno] effortlessly flings [carbon] to the side!"), SPAN_XENOWARNING("We effortlessly fling [carbon] to the side!"))
 	playsound(carbon,'sound/weapons/alien_claw_block.ogg', 75, 1)
 	if(stun_power)
-		carbon.apply_effect(get_xeno_stun_duration(carbon, stun_power), STUN)
+		carbon.Stun(get_xeno_stun_duration(carbon, stun_power))
 	if(weaken_power)
-		carbon.apply_effect(weaken_power, WEAKEN)
+		carbon.KnockDown(get_xeno_stun_duration(carbon, weaken_power))
 	if(slowdown)
 		if(carbon.slowed < slowdown)
 			carbon.apply_effect(slowdown, SLOW)

code/modules/mob/living/carbon/xenomorph/castes/Warrior.dm
@@ -101,7 +101,9 @@
 
 		if(should_neckgrab && living_mob.mob_size < MOB_SIZE_BIG)
 			living_mob.drop_held_items()
-			living_mob.apply_effect(get_xeno_stun_duration(living_mob, 2), WEAKEN)
+			var/duration = get_xeno_stun_duration(living_mob, 2)
+			living_mob.KnockDown(duration)
+			living_mob.Stun(duration)
 			if(living_mob.pulledby != src)
 				return // Grab was broken, probably as Stun side effect (eg. target getting knocked away from a manned M56D)
 			visible_message(SPAN_XENOWARNING("[src] grabs [living_mob] by the throat!"), \

cmss13-devs/cmss13-pve

code/modules/mob/living/carbon/xenomorph/abilities/warrior/warrior_powers.dm
@@ -94,9 +94,9 @@
 	woyer.visible_message(SPAN_XENOWARNING("\The [woyer] effortlessly flings [carbone] to the side!"), SPAN_XENOWARNING("You effortlessly fling [carbone] to the side!"))
 	playsound(carbone,'sound/weapons/alien_claw_block.ogg', 75, 1)
 	if(stun_power)
-		carbone.apply_effect(get_xeno_stun_duration(carbone, stun_power), STUN)
+		carbone.Stun(get_xeno_stun_duration(carbone, stun_power))
 	if(weaken_power)
-		carbone.apply_effect(weaken_power, WEAKEN)
+		carbone.KnockDown(get_xeno_stun_duration(carbon, weaken_power))
 	if(slowdown)
 		if(carbone.slowed < slowdown)
 			carbone.apply_effect(slowdown, SLOW)

code/modules/mob/living/carbon/xenomorph/castes/Warrior.dm
@@ -95,7 +95,9 @@
 
 		if(should_neckgrab && L.mob_size < MOB_SIZE_BIG)
 			L.drop_held_items()
-			L.apply_effect(get_xeno_stun_duration(L, 2), WEAKEN)
+			var/duration = get_xeno_stun_duration(L, 2)
+			L.KnockDown(duration)
+			L.Stun(duration)
 			L.pulledby = src
 			visible_message(SPAN_XENOWARNING("\The [src] grabs [L] by the throat!"), \
 			SPAN_XENOWARNING("You grab [L] by the throat!"))