cmss13-devs/cmss13

React Migration - part 2 (#5493)
# About the pull request

<!-- Remove this text and explain what the purpose of your PR is.

Mention if you have tested your changes. If you changed a map, make sure
you used the mapmerge tool.
If this is an Issue Correction, you can type "Fixes Issue #169420" to
link the PR to the corresponding Issue number #169420.

Remember: something that is self-evident to you might not be to others.
Explain your rationale fully, even if you feel it goes without saying.
-->

Switches TGUI from Inferno to React.

Part 2 of the PR https://github.com/cmss13-devs/cmss13/pull/5435

Based heavily on the work from:
https://github.com/tgstation/tgstation/pull/80044/files

# Explain why it's good for the game
React is a more suitable framework

# Testing Photographs and Procedure
<details>
<summary>Screenshots & Videos</summary>

Put screenshots and videos here with an empty line between the
screenshots and the `<details>` tags.

</details>


# Changelog
:cl:
refactor: switched from infernojs to react
/:cl:

tgui/packages/tgui/interfaces/SquadInfo.tsx
@@ -47,11 +47,8 @@ interface SquadProps {
   objective: { primary?: string; secondary?: string };
 }
 
-const FireTeamLeadLabel = (
-  props: { readonly ftl: SquadMarineEntry },
-  context
-) => {
-  const { data } = useBackend<SquadProps>(context);
+const FireTeamLeadLabel = (props: { readonly ftl: SquadMarineEntry }) => {
+  const { data } = useBackend<SquadProps>();
   const { ftl } = props;
   return (
     <>
@@ -75,11 +72,11 @@ const FireTeamLeadLabel = (
   );
 };
 
-const FireTeamLead = (
-  props: { readonly fireteam: FireTeamEntry; readonly ft: string },
-  context
-) => {
-  const { data, act } = useBackend<SquadProps>(context);
+const FireTeamLead = (props: {
+  readonly fireteam: FireTeamEntry;
+  readonly ft: string;
+}) => {
+  const { data, act } = useBackend<SquadProps>();
   const fireteamLead = props.fireteam.tl;
   const isNotAssigned =
     fireteamLead === undefined ||
@@ -114,7 +111,7 @@ interface FireteamBoxProps extends BoxProps {
   readonly isEmpty: boolean;
 }
 
-const FireteamBox = (props: FireteamBoxProps, context) => {
+const FireteamBox = (props: FireteamBoxProps) => {
   return (
     <Box className={classes(['FireteamBox'])}>
       <div className="Title">{props.name}</div>
@@ -123,8 +120,8 @@ const FireteamBox = (props: FireteamBoxProps, context) => {
   );
 };
 
-const FireTeam = (props: { readonly ft: string }, context) => {
-  const { data, act } = useBackend<SquadProps>(context);
+const FireTeam = (props: { readonly ft: string }) => {
+  const { data, act } = useBackend<SquadProps>();
   const fireteam: FireTeamEntry = data.fireteams[props.ft];
 
   const members: SquadMarineEntry[] =
@@ -193,15 +190,12 @@ const FireTeam = (props: { readonly ft: string }, context) => {
   );
 };
 
-const FireTeamMember = (
-  props: {
-    readonly member: SquadMarineEntry;
-    readonly team: string;
-    readonly fireteam?: FireTeamEntry;
-  },
-  context
-) => {
-  const { data, act } = useBackend<SquadProps>(context);
+const FireTeamMember = (props: {
+  readonly member: SquadMarineEntry;
+  readonly team: string;
+  readonly fireteam?: FireTeamEntry;
+}) => {
+  const { data, act } = useBackend<SquadProps>();
   const assignFT1 = { target_ft: 'FT1', target_marine: props.member.name };
   const assignFT2 = { target_ft: 'FT2', target_marine: props.member.name };
   const assignFT3 = { target_ft: 'FT3', target_marine: props.member.name };
@@ -273,8 +267,8 @@ const FireTeamMember = (
   );
 };
 
-const SquadObjectives = (props, context) => {
-  const { data } = useBackend<SquadProps>(context);
+const SquadObjectives = (props) => {
+  const { data } = useBackend<SquadProps>();
   const primaryObjective = data.objective?.primary ?? 'Unset';
   const secondaryObjective = data.objective?.secondary ?? 'Unset';
   return (
@@ -289,8 +283,8 @@ const SquadObjectives = (props, context) => {
   );
 };
 
-export const SquadInfo = (_, context) => {
-  const { data } = useBackend<SquadProps>(context);
+export const SquadInfo = () => {
+  const { data } = useBackend<SquadProps>();
   const fireteams = ['FT1', 'FT2', 'FT3', 'Unassigned'];
 
   return (

cmss13-devs/cmss13-pve

tgui/packages/tgui/interfaces/SquadInfo.tsx
@@ -47,11 +47,8 @@ interface SquadProps {
   objective: { primary?: string; secondary?: string };
 }
 
-const FireTeamLeadLabel = (
-  props: { readonly ftl: SquadMarineEntry },
-  context
-) => {
-  const { data } = useBackend<SquadProps>(context);
+const FireTeamLeadLabel = (props: { readonly ftl: SquadMarineEntry }) => {
+  const { data } = useBackend<SquadProps>();
   const { ftl } = props;
   return (
     <>
@@ -75,11 +72,11 @@ const FireTeamLeadLabel = (
   );
 };
 
-const FireTeamLead = (
-  props: { readonly fireteam: FireTeamEntry; readonly sqsgt: string },
-  context
-) => {
-  const { data, act } = useBackend<SquadProps>(context);
+const FireTeamLead = (props: {
+  readonly fireteam: FireTeamEntry;
+  readonly sqsgt: string;
+}) => {
+  const { data, act } = useBackend<SquadProps>();
   const fireteamLead = props.fireteam.sqsgt;
   const isNotAssigned =
     fireteamLead === undefined ||
@@ -114,7 +111,7 @@ interface FireteamBoxProps extends BoxProps {
   readonly isEmpty: boolean;
 }
 
-const FireteamBox = (props: FireteamBoxProps, context) => {
+const FireteamBox = (props: FireteamBoxProps) => {
   return (
     <Box className={classes(['FireteamBox'])}>
       <div className="Title">{props.name}</div>
@@ -123,8 +120,8 @@ const FireteamBox = (props: FireteamBoxProps, context) => {
   );
 };
 
-const FireTeam = (props: { readonly sqsgt: string }, context) => {
-  const { data, act } = useBackend<SquadProps>(context);
+const FireTeam = (props: { readonly sqsgt: string }) => {
+  const { data, act } = useBackend<SquadProps>();
   const fireteam: FireTeamEntry = data.fireteams[props.sqsgt];
 
   const members: SquadMarineEntry[] =
@@ -193,15 +190,12 @@ const FireTeam = (props: { readonly sqsgt: string }, context) => {
   );
 };
 
-const FireTeamMember = (
-  props: {
-    readonly member: SquadMarineEntry;
-    readonly team: string;
-    readonly fireteam?: FireTeamEntry;
-  },
-  context
-) => {
-  const { data, act } = useBackend<SquadProps>(context);
+const FireTeamMember = (props: {
+  readonly member: SquadMarineEntry;
+  readonly team: string;
+  readonly fireteam?: FireTeamEntry;
+}) => {
+  const { data, act } = useBackend<SquadProps>();
   const assignFT1 = { target_ft: 'SQ1', target_marine: props.member.name };
   const assignFT2 = { target_ft: 'SQ2', target_marine: props.member.name };
 
@@ -269,8 +263,8 @@ const FireTeamMember = (
   );
 };
 
-const SquadObjectives = (props, context) => {
-  const { data } = useBackend<SquadProps>(context);
+const SquadObjectives = (props) => {
+  const { data } = useBackend<SquadProps>();
   const primaryObjective = data.objective?.primary ?? 'Unset';
   const secondaryObjective = data.objective?.secondary ?? 'Unset';
   return (
@@ -285,8 +279,8 @@ const SquadObjectives = (props, context) => {
   );
 };
 
-export const SquadInfo = (_, context) => {
-  const { config, data } = useBackend<SquadProps>(context);
+export const SquadInfo = () => {
+  const { config, data } = useBackend<SquadProps>();
   const fireteams = ['SQ1', 'SQ2', 'Unassigned'];
 
   return (