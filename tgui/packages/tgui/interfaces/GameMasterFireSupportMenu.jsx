bfe87d018982f1d6015f1cf0e2ebbba7860050f4
SunoFang <opblmc@gmail.com>
2024-08-07 20:41:28 +0200

    JSXifies fire support menu. Yippie!

--- tgui/packages/tgui/interfaces/GameMasterFireSupportMenu.jsx
+++ tgui/packages/tgui/interfaces/GameMasterFireSupportMenu.jsx
@@ -1,10 +1,11 @@
 import { auto } from '@popperjs/core';
+
 import { useBackend } from '../backend';
 import { Button, Collapsible, Section, Stack } from '../components';
 import { Window } from '../layouts';
 
 export const GameMasterFireSupportMenu = (props, context) => {
-  const { act, data } = useBackend(context);
+  const { act, data } = useBackend();
   return (
     <Window resizable width={'450px'}>
       <Window.Content scrollable>
@@ -13,7 +14,8 @@ export const GameMasterFireSupportMenu = (props, context) => {
           title="Fire Support Menu"
           align="center"
           justify="center"
-          height={auto}>
+          height={auto}
+        >
           <Stack vertical>
             <Stack.Item>
               <Button
@@ -21,67 +23,72 @@ export const GameMasterFireSupportMenu = (props, context) => {
                 fontSize="15px"
                 ml={1}
                 selected={data.fire_support_click_intercept}
-                content="CALL FIRE SUPPORT"
                 onClick={() => {
                   act('toggle_click_fire_support');
                 }}
-              />
+              >
+                CALL FIRE SUPPORT
+              </Button>
             </Stack.Item>
           </Stack>
 
           <Collapsible content="Missiles">
             {data.missile_ordnance_options.map((ordnance, i) => (
               <Button
-                content={ordnance}
                 selected={data.selected_ordnance === ordnance}
                 key={i}
                 width={'140px'}
                 onClick={() => {
                   act('set_selected_ordnance', { ordnance });
                 }}
-              />
+              >
+                {ordnance}
+              </Button>
             ))}
           </Collapsible>
 
           <Collapsible content="Orbital Bombardments">
             {data.orbital_ordnance_options.map((ordnance, i) => (
               <Button
-                content={ordnance}
                 selected={data.selected_ordnance === ordnance}
                 key={i}
                 width={'140px'}
                 onClick={() => {
                   act('set_selected_ordnance', { ordnance });
                 }}
-              />
+              >
+                {ordnance}
+              </Button>
             ))}
           </Collapsible>
 
           <Collapsible content="Mortar Shells">
             {data.mortar_ordnance_options.map((ordnance, i) => (
               <Button
-                content={ordnance}
                 selected={data.selected_ordnance === ordnance}
                 key={i}
                 width={'140px'}
                 onClick={() => {
                   act('set_selected_ordnance', { ordnance });
                 }}
-              />
+              >
+                {ordnance}
+              </Button>
             ))}
           </Collapsible>
 
           <Collapsible content="Misc Ordnance">
             {data.misc_ordnance_options.map((ordnance, i) => (
               <Button
-                content={ordnance}
                 selected={data.selected_ordnance === ordnance}
                 key={i}
                 width={'140px'}
                 onClick={() => {
                   act('set_selected_ordnance', { ordnance });
                 }}
-              />
+              >
+                {ordnance}
+              </Button>
             ))}
           </Collapsible>
         </Section>
