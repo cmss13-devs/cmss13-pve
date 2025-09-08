20926251682c4ef48ae456039b8ef48f4780efab
Drathek <76988376+Drulikar@users.noreply.github.com>
2025-09-08 12:41:32 -0500

    Bump sdmm suite for linting to 1.11 (#10542)
    Bumps spacemandmm suite to 1.11 for
    https://github.com/SpaceManiac/SpacemanDMM/releases/tag/suite-1.11
    
    Bringing linters back on parity with local dev tools in VSC.
    
    <details>
    <summary>Screenshots & Videos</summary>
    
    Put screenshots and videos here with an empty line between the
    screenshots and the `<details>` tags.
    
    </details>
    
    :cl: Drathek
    code: SDMM suite for linting is bumped to a new version for more 516
    support
    /:cl:

--- dependencies.sh
+++ dependencies.sh
@@ -14,7 +14,7 @@ export RUST_G_VERSION=4.2.0
 export NODE_VERSION_LTS=20.12.0
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.8
+export SPACEMAN_DMM_VERSION=suite-1.11
 
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.11.9
